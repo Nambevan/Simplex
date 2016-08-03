0d9e677e2eb77d2756674e00ced711635d4e0473
Nambevan <mag_got@i.ua>
2016-08-03 21:31:41 +0300

    #3

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
+    $('.Home').click(function () {
+        $('.main_bg').css('background-image','url("img/background.png")');
+        $('.home_content').css('display','block');
+        $('.about_content').css('display','none');
+        $('.service_content').css('display','none');
+        $('.clients_content').css('display','none');
+
+        $('.Home').css('background-color','#3f9fc9');
+        $('.About').css('background-color','transparent');
+        $('.Services').css('background-color','transparent');
+        $('.Clients').css('background-color','transparent');
+        $('.Contacts').css('background-color','transparent');
+    });
+    
     $('.About').click(function () { 
         $('.main_bg').css('background-image','none');
         $('.main_bg').css('background-color','#f17c72');
         $('.home_content').css('display','none');
+        $('.about_content').css('display','block');
+        $('.service_content').css('display','none');
+        $('.clients_content').css('display','none');
 
+        $('.Home').css('background-color','transparent');
         $('.About').css('background-color','#e6655e');
         $('.Services').css('background-color','transparent');
         $('.Clients').css('background-color','transparent');
@@ -14,7 +32,11 @@ $(document).ready(function() {
         $('.main_bg').css('background-image','none');
         $('.main_bg').css('background-color','#32ac97');
         $('.home_content').css('display','none');
+        $('.about_content').css('display','none');
+        $('.service_content').css('display','block');
+        $('.clients_content').css('display','none');
 
+        $('.Home').css('background-color','transparent');
         $('.About').css('background-color','transparent');
         $('.Services').css('background-color','#46dcc7');
         $('.Clients').css('background-color','transparent');
@@ -25,7 +47,11 @@ $(document).ready(function() {
         $('.main_bg').css('background-image','none');
         $('.main_bg').css('background-color','#7f4c76');
         $('.home_content').css('display','none');
+        $('.about_content').css('display','none');
+        $('.service_content').css('display','none');
+        $('.clients_content').css('display','block');
 
+        $('.Home').css('background-color','transparent');
         $('.About').css('background-color','transparent');
         $('.Services').css('background-color','transparent');
         $('.Clients').css('background-color','#c7a1c2');
@@ -37,7 +63,11 @@ $(document).ready(function() {
         $('.main_bg').css('background-image','none');
         $('.main_bg').css('background-color','#3f9fc9');
         $('.home_content').css('display','none');
+        $('.about_content').css('display','none');
+        $('.service_content').css('display','none');
+        $('.clients_content').css('display','none');
 
+        $('.Home').css('background-color','transparent');
         $('.About').css('background-color','transparent');
         $('.Services').css('background-color','transparent');
         $('.Clients').css('background-color','transparent');
@@ -45,15 +75,5 @@ $(document).ready(function() {
 
     });
 
-    $('.Home').click(function () {
-        $('.main_bg').css('background-image','url("img/backgroung.png")');
-        $('.home_content').css('display','block');
-
-        $('.Home').css('background-color','#3f9fc9');
-        $('.About').css('background-color','transparent');
-        $('.Services').css('background-color','transparent');
-        $('.Clients').css('background-color','transparent');
-        $('.Contacts').css('background-color','transparent');
-    });
 })
 
